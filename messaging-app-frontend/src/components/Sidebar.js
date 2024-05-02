4add1d05b409ba5b32b626f682dca7771dbd4165
xmarcjordanx <mhujanen1@gmail.com>
2024-05-01 21:21:45 -0600

    frontend updates

--- messaging-app-frontend/src/components/Sidebar.js
+++ messaging-app-frontend/src/components/Sidebar.js
@@ -1,12 +1,38 @@
 import React from 'react'
+import { Avatar, IconButton } from '@mui/material'
+import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import ChatIcon from '@mui/icons-material/Chat'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+import { SearchOutlined } from '@mui/icons-material'
 import './Sidebar.css'
+import SidebarChat from './SidebarChat'
+
 const Sidebar = () => {
     return (
-        <div>
-            <div className="sidebar__header"></div>
-            <div className="sidebar__search"></div>
-            <div className="sidebar__chats"></div>
-            
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input type="text" placeholder="Search or start new chat" />
+                </div>
+            </div>
+            <div className="sidebar__chats">
+                <SidebarChat />
+            </div>
         </div>
     )
 }
