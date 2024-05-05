fa3c57d6fa1ec7f7a431561fe272c7e51123df68
xmarcjordanx <mhujanen1@gmail.com>
2024-05-04 22:51:49 -0600

    update

--- messaging-app-frontend/src/components/axios.js
+++ messaging-app-frontend/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:"http://localhost:9000"
+    baseURL:"https://messaging-app-mern-8a4089593683.herokuapp.com"
 })
 
 export default instance
